1ae49fc60ef526e2e450eb8499ce68621ecf2d0f
EllisAC <ellisac@jncb.com>
2021-11-17 19:17:03 -0500

    Data Structure - Array

---
+++ src/com/company/DSArray.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class DSArray {
+    public static void main(String[] args) {
+         // Mocking a chess board
+
+        int boardDim = 8;
+        char[][] board = new char[boardDim][10];
+        boolean isWhite = false;
+
+        System.out.println("Board Length: " + board.length);
+
+        for(int y=0; y < board.length; y++){
+            isWhite = !isWhite;
+            System.out.println(board[y].length);
+
+//          The below for loop will be iterating through the individual elements of rows.
+            for (int x = 0; x < board[y].length ; x++) {
+                if (isWhite) board[y][x] = 'W';
+                if (!isWhite) board[y][x] = 'B';
+                isWhite = !isWhite;
+            }
+
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            System.out.println(Arrays.toString(board[i]));
+        }
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+//            Control FLow
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String stringVar = scan.next();
+        System.out.println(stringVar);
+
+        int x = 1;
+
+        switch(x){
+            case 1:
+                System.out.println("RED");
+                break;
+            case 2: case 4: // YES WE CAN PUT MULTIPLE CASES ON ONE LINE. THE CASES FREELY FALL THROUGH TO EACH OTHER, AND THIS IS WHY WE CAN DO THIS.
+                System.out.println("BLUE");
+                break;
+            case 3:
+                System.out.println("YELLOW");
+                break;
+            default:
+                System.out.println("YOYO");
+
+        }
     }
 }
